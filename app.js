c1a5609806672594397c61b1b941b7f9e5574884
Jeremy Marino <jermarino16@gmail.com>
2020-02-15 03:51:14 +0000

    refactoring display page of one campground, resized images, hid google maps on small screen sizes

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(indexRoutes);
 app.use(commentRoutes);
 app.use(campgroundRoutes);
 
-seedDB(); //clear database
+// seedDB(); //clear database
 
 app.listen(process.env.PORT || 3000, process.env.ip, function(){
 	console.log("Yelp Server has started");
